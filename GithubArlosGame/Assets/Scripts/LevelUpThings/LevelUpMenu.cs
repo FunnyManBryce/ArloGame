a29600f3f9b426ec475a71d222e0ad06998993e7
SplintRcast <emilianojude@gmail.com>
2024-02-25 16:04:10 -0800

    FredyF -azbear

--- GithubArlosGame/Assets/Scripts/LevelUpThings/LevelUpMenu.cs
+++ GithubArlosGame/Assets/Scripts/LevelUpThings/LevelUpMenu.cs
@@ -18,32 +18,54 @@ public class LevelUpMenu : RestartScene
     {
         InitializeMenu();
     }
+    public void ResetAndApplyEffects(Button clickedButton)
+    {
+        foreach (Button button in buttons)
+        {
+            button.onClick.RemoveAllListeners();
+
+            TextMeshProUGUI buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
+            
+            LevelUpEffect defaultEffect = button.GetComponent<LevelUpEffect>();
+            if (defaultEffect != null)
+            {
+
+                if (buttonText != null)
+                {
+                    buttonText.text = defaultEffect.buttonText;
+                }
+            }
+
+            LevelUpEffect randomEffect = levelUpEffects[Random.Range(0, levelUpEffects.Count)];
+            if (buttonText != null)
+            {
+                buttonText.text = randomEffect.buttonText;
+            }
+            button.onClick.AddListener(() => randomEffect.ApplyEffect());
+        }
+    }
 
     public void InitializeMenu()
     {
-
         if (buttons.Count == 0 || levelUpEffects.Count == 0)
         {
             Debug.LogWarning("Buttons or level up effects are not assigned.");
             return;
         }
 
-        for (int i = 0; i < buttons.Count; i++)
+        foreach (Button button in buttons)
         {
-            LevelUpEffect randomEffect = levelUpEffects[Random.Range(0, levelUpEffects.Count)];
-
-            TMPro.TMP_Text buttonText = buttons[i].GetComponentInChildren<TMPro.TMP_Text>();
-
-            if (buttonText != null)
+            LevelUpEffect defaultEffect = button.GetComponent<LevelUpEffect>();
+            if (defaultEffect != null)
             {
-                buttonText.text = randomEffect.buttonText;  
-            }   
-            else
-            {
-                Debug.LogWarning("Text component not found on the button.");
+                TextMeshProUGUI buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
+                if (buttonText != null)
+                {
+                    buttonText.text = defaultEffect.buttonText;
+                }
             }
 
-            buttons[i].onClick.AddListener(() => randomEffect.ApplyEffect());
+            ResetAndApplyEffects(button);
         }
     }
 }
